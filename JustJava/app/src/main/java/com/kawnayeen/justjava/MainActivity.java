e123668cf8f250a1b8a18417210368b2a540a2b3
kawnayeen <kawnayeen.08@gmail.com>
2017-05-09 13:02:21 +0600

    when tapping any other places except edittext, keyboard will be hidden

--- JustJava/app/src/main/java/com/kawnayeen/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/kawnayeen/justjava/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kawnayeen.justjava;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -28,11 +31,15 @@ public class MainActivity extends AppCompatActivity {
     Button decrementBtn;
     @BindView(R.id.whipped_cream_topping)
     CheckBox whippedCream;
+    @BindView(R.id.chocolate_topping)
+    CheckBox chocolate;
     @BindView(R.id.et_customer_name)
     EditText customerName;
 
     private int numberOfCoffees;
-    private String name = "Kawnayeen";
+    private static final int BASE_PRICE = 5;
+    private static final int WHIPPED_CREAM_PRICE = 1;
+    private static final int CHOCOLATE_PRICE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,41 +48,59 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         numberOfCoffees = 2;
         orderBtn.setOnClickListener(v -> submitOrder());
-        incrementBtn.setOnClickListener(v -> incrementOrder());
-        decrementBtn.setOnClickListener(v -> decrementOrder());
+        incrementBtn.setOnClickListener(this::incrementOrder);
+        decrementBtn.setOnClickListener(this::decrementOrder);
+        whippedCream.setOnClickListener(this::updateQuantityAndPrice);
+        chocolate.setOnClickListener(this::updateQuantityAndPrice);
+        customerName.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus)
+                hideKeyboard(v);
+        });
     }
 
-    private void incrementOrder() {
+    private void incrementOrder(View v) {
         numberOfCoffees++;
-        updateQuantityAndPrice();
+        updateQuantityAndPrice(v);
     }
 
-    private void decrementOrder() {
+    private void decrementOrder(View v) {
         if (numberOfCoffees > 0) {
             numberOfCoffees--;
-            updateQuantityAndPrice();
+            updateQuantityAndPrice(v);
         }
     }
 
     private void submitOrder() {
-        name = customerName.getText().toString();
+        String name = customerName.getText().toString();
         String str = "";
         str += "Name : " + name;
         if (whippedCream.isChecked())
             str += "\nAdd whipped cream";
+        if (chocolate.isChecked())
+            str += "\nAdd chocolate";
         str += "\nQuantity : " + numberOfCoffees;
         str += "\nTotal :" + NumberFormat.getCurrencyInstance().format(calculatePrice());
         str += "\nThank you!";
         displayMessage(str);
     }
 
-    private void updateQuantityAndPrice() {
+    private void updateQuantityAndPrice(View v) {
         setQuantityText(numberOfCoffees);
         displayMessage(NumberFormat.getCurrencyInstance().format(calculatePrice()));
+        hideKeyboard(v);
     }
 
     private int calculatePrice() {
-        return numberOfCoffees * 5;
+        return numberOfCoffees * getPricePerCup();
+    }
+
+    private int getPricePerCup() {
+        int price = BASE_PRICE;
+        if (whippedCream.isChecked())
+            price += WHIPPED_CREAM_PRICE;
+        if (chocolate.isChecked())
+            price += CHOCOLATE_PRICE;
+        return price;
     }
 
     private void setQuantityText(int quantity) {
@@ -84,5 +109,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayMessage(String message) {
         priceTv.setText(message);
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
